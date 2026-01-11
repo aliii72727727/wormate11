1d6aa101c30cbc1d8fbd45f5e026eeedd1960567
ABSO <atargaming901@gmail.com>
2026-01-11 17:05:01 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -165,7 +165,7 @@ async function loadUsers() {
 }
 
 async function loadServers() {
-    await fetch("https://wormps.github.io/extension/api/server.php")
+    await fetch("https://aliii72727727.github.io/wormate11/api/server.php")
         .then(response => response.json())
         .then(response => {
             if (response.success) {
